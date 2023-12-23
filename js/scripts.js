b265257e363302da233b0e1079d58c8233c20517
lomovlena <151032586+lomovlena@users.noreply.github.com>
2023-12-23 14:18:35 +0100

    Create new repository

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+alert('Hello World');
+
+let myName ='Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let simpleAdditon = 2 + 2;
+document.write(simpleAdditon);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+let favoriteFood = 'Mango';
+document.write(favoriteFood);
